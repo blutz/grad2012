61f82eba9f03fc196e8ac915c7b9391f5c5bc00e
Byron Lutz <byronlutz@gmail.com>
2012-06-11 00:55:55 -0700

    Reorder pages

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 $validPages = array(
 	'home' => 'Home',
-	'graduation-columns' => 'Graduation Columns',
-	'ae-profiles' => 'a&e Profiles',
-	'day-in-the-life' => 'Day in the Life',
-	'then-and-now' => 'Then and Now',
 	'map-of-memories' => 'Map of Memories',
 	'summer-sports' => 'Summer Sports',
 	'endangered-languages' => 'Endangered Languages',
+	'day-in-the-life' => 'Day in the Life',
+	'then-and-now' => 'Then and Now',
+	'ae-profiles' => 'a&e Profiles',	
+	'graduation-columns' => 'Graduation Columns',
 );
 
 if(isset($_GET['p']) && array_key_exists($_GET['p'],$validPages)){
