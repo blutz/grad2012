d61dfa63028d786de370298700e86e5d3b938f60
Byron Lutz <byronlutz@gmail.com>
2012-06-11 06:56:27 -0700

    Then and Now captions and images added

---
+++ pages/then-and-now.php
@@ -0,0 +1,44 @@
+<div class="gallery">
+
+	<div id="galleria" style="height:100%">
+		<img src="content/then-and-now/thenAndNowFront.jpg"></img>
+
+
+		<a href="content/then-and-now/isaacFront.jpg"><img src="content/then-and-now/thumb/isaacFront.jpg" data-title="During one of my first nights as an editor at the Daily Bruin office last fall quarter, I began searching through our photo archives and stumbled across many interesting photographs from UCLA’s past. The images varied from construction to daily campus life; each one showed a frame of how much the campus has progressed and developed since its early days in Westwood. Throughout the year, I’ve spent hours sifting through the archives looking for photos that could visually describe how the campus has progressed and evolved over time. Several hours were spent going through the stack of pictures kept in a couple of old boxes.<br /><br />
+Being able to revisit these different eras through the pictures and intertwining them with the modern landscape is what drew me to this project. Rather than having a gallery of historical photos of the campus, the combination of both gives context on the change the campus has experienced over the years. As a graduating senior, it’s humbling to realize how much history the campus has and the large impact that the student body has had in the development of it. Through this project I sought to help foster an appreciation for the university and to allow our readers to embrace the history captured in these photos. The photos in this essay help to bridge the gap between the then and now. Enjoy the photo essay and thanks for picking up the newspaper every day." data-description="&mdash;Isaac Arjonilla, Daily Bruin photo editor 2011-2012"></img></a>
+
+		<a href="content/then-and-now/web.ph.6.11.then&nowarjonilla.picB.jpg"><img data-title="Kerckhoff Hall renovations in early 1999 involved the destruction of the patio and leveling of the lower floors." src="content/then-and-now/thumb/web.ph.6.11.then&nowarjonilla.picB.jpg"></img></a>
+
+		<a href="content/then-and-now/web.ph.6.11.then&nowarjonilla.picA.jpg"><img data-title="Kerckhoff Hall then and now, as seen in a Daily Bruin file photo." src="content/then-and-now/thumb/web.ph.6.11.then&nowarjonilla.picA.jpg"></img></a>
+		
+		<a href="content/then-and-now/web.ph.6.11.then&nowarjonilla.picD.jpg"><img data-title="Powell Library then and now. Powell Library is unveiled to the students after seismic remodeling in May 1993." src="content/then-and-now/thumb/web.ph.6.11.then&nowarjonilla.picD.jpg"></img></a>
+		
+		<a href="content/then-and-now/web.ph.6.11.then&nowarjonilla.picC.jpg"><img data-title="Royce Hall then and now. In October 1992, students laid down in makeshift graves in front of Royce Hall to protest budget hikes." src="content/then-and-now/thumb/web.ph.6.11.then&nowarjonilla.picC.jpg"></img></a>
+
+
+
+		<a href="content/then-and-now/melindaFront.jpg"><img src="content/then-and-now/thumb/melindaFront.jpg" data-title="As lame as it sounds, I love waking up every morning and going to UCLA. From the lush green lawns, to the towering brick structures, to the arches of Royce Hall, to the hushed silence in Powell Library, to the brutal hike up Janss Steps, to the freshly printed Daily Bruins, to the Inverted Fountain that strangely always reminded me of an ever-circulating toilet, to the brilliant scholars that have blown my mind into baffled awe and astonishment, to the people, who I think truly make UCLA great.<br /><br />
+You can be of any social group, race, ethnicity, culture, sexual orientation, religion and socio-economic status, and you will feel welcome at our school. Students enthusiastically dive into UCLA's diverse culture within their studies, classrooms, clubs and everywhere in between. But, what I really love is that people want to learn about our lives – our unique stories and histories that have brought us all to UCLA. <br /><br />
+Long ago, our acceptance letters stated that more often than we might imagine, &ldquo;UCLA will be where the world learns about you.&rdquo; I didn’t really get it then, but now I do. UCLA isn’t only our school, but a place where we can be ourselves, and express our thoughts to open and friendly minds. It is where we can grow and be the blooming buds, and it’s also where we can inspire and be the nourishing water. I’ve loved every moment of my time spent here, and with the acceptance and encouragement of my peers and professors, I graduate UCLA as a true Bruin, but also as a confident and vibrant individual who is excited to explore the world we live in. Thanks UCLA, it's been a blast!<br />&mdash;Melinda Minoofar, graduating senior<br /><br />EDITOR'S NOTE<br /><br />This photo series was produced by Melinda Minoofar, a graduating English student who submitted her pictures to the Daily Bruin.<br /><br />
+This quarter, Minoofar took several pictures that overlay old images with UCLA’s current landscape as part of a personal project. The pictures of UCLA's past were taken between the 1920s and 1960s. Since then, UCLA has changed in dramatic ways, from the number of students to the majors offered. These pictures provide a glimpse into that long-gone campus&mdash;within the perspective of the campus as it is today. Accompanying the photos, Minoofar also submitted a few paragraphs to summarize her experience at UCLA."></img></a>
+
+
+		<a href="content/then-and-now/web.6.11.minoofar.picC.jpg"><img data-title="Students jump into the center of the Inverted Fountain located outside of Franz Hall." src="content/then-and-now/thumb/web.6.11.minoofar.picC.jpg"></img></a>
+		
+		<a href="content/then-and-now/web.6.11.minoofar.picB.jpg"><img data-title="Royce Hall then and now. Students protest in front of Royce Hall." src="content/then-and-now/thumb/web.6.11.minoofar.picB.jpg"></img></a>
+		
+		<a href="content/then-and-now/web.6.11.minoofar.picA.jpg"><img data-title="Powell Library then and now. A view of Powell Library is shown through the arches of Royce Hall. " src="content/then-and-now/thumb/web.6.11.minoofar.picA.jpg"></img></a>
+
+		<a href="content/then-and-now/web.6.11.minoofar.picD.jpg"><img data-title="Dickson Plaza then and now. Students walk through Dickson Plaza with books in hand." src="content/then-and-now/thumb/web.6.11.minoofar.picD.jpg"></img></a>
+
+
+
+
+
+<!-- 		<a href="content/then-and-now/"><img data-title="title" data-description="description" src="content/then-and-now/thumb/"></img></a> -->
+
+
+
+
+	</div>
+</div>
