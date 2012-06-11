741fe97ddff79bcb3a334633b93d4b3881511377
Byron Lutz <byronlutz@gmail.com>
2012-06-11 00:56:09 -0700

    Day in the life images added

--- pages/day-in-the-life.php
+++ pages/day-in-the-life.php
@@ -1,6 +1,26 @@
 <div class="gallery">
 
 	<div id="galleria" style="height:100%">
-		<img src="http://placekitten.com/500/500"></img>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.2-4pm.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.2-4pm.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.2-4pm.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.2-4pm.picB.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.2-4pm.picC.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.2-4pm.picC.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.3-6am.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.3-6am.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.3-6am.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.3-6am.picB.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.4-6pm.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.4-6pm.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.4-6pm.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.4-6pm.picB.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.4-6pm.picC.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.4-6pm.picC.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.6-9am.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.6-9am.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.6-9am.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.6-9am.picB.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.6-9pm.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.6-9pm.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.6-9pm.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.6-9pm.picB.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.9-11am.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.9-11am.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.9-11am.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.9-11am.picB.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.10-12.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.10-12.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.10-12pm.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.10-12pm.picB.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.11-2pm.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.11-2pm.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.11-2pm.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.11-2pm.picB.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.11-2pm.picC.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.11-2pm.picC.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.12-3am.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.12-3am.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.12-3am.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.12-3am.picB.jpg"></img></a>
 	</div>
 </div>
