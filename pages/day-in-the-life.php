8238cf815ba1d40a427e14a74ced0db9cffe81c9
Byron Lutz <byronlutz@gmail.com>
2012-06-11 06:16:48 -0700

    Day in the Life captions/info placed

--- pages/day-in-the-life.php
+++ pages/day-in-the-life.php
@@ -1,26 +1,34 @@
 <div class="gallery">
 
 	<div id="galleria" style="height:100%">
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.2-4pm.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.2-4pm.picA.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.2-4pm.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.2-4pm.picB.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.2-4pm.picC.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.2-4pm.picC.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.3-6am.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.3-6am.picA.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.3-6am.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.3-6am.picB.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.4-6pm.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.4-6pm.picA.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.4-6pm.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.4-6pm.picB.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.4-6pm.picC.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.4-6pm.picC.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.6-9am.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.6-9am.picA.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.6-9am.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.6-9am.picB.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.6-9pm.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.6-9pm.picA.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.6-9pm.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.6-9pm.picB.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.9-11am.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.9-11am.picA.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.9-11am.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.9-11am.picB.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.10-12.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.10-12.picA.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.10-12pm.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.10-12pm.picB.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.11-2pm.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.11-2pm.picA.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.11-2pm.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.11-2pm.picB.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.11-2pm.picC.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.11-2pm.picC.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.12-3am.picA.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.12-3am.picA.jpg"></img></a>
-		<a href="content/day-in-the-life/web.ph.6.11.DITL.12-3am.picB.jpg"><img src="content/day-in-the-life/thumb/web.ph.6.11.DITL.12-3am.picB.jpg"></img></a>
+		<img src="content/day-in-the-life/dayInTheLifeFront.jpg"></img>
+
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.6-9am.picA.jpg"><img data-title="8:18 a.m. near The Hill" data-description="A student's teddy bear displays the day's weather forecast.<br />CASIE NGUYEN/Daily Bruin" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.6-9am.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.6-9am.picB.jpg"><img data-title="6:40 a.m. near Covel Commons" data-description="Third-year communication studies student and former Daily Bruin copy contributor Hillary Jacobsen walks down Covel steps after grabbing a drink from Bruin Cafe.<br />CASIE NGUYEN/Daily Bruin" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.6-9am.picB.jpg"></img></a>
+
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.9-11am.picA.jpg"><img data-title="9:27 a.m. near the UCLA Store" data-description="Third-year political science Yamili Gonzalez folds sweaters at the UCLA store in Ackerman Union.<br />ZOE ERSKINE/Daily Bruin" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.9-11am.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.9-11am.picB.jpg"><img data-title="9:52 a.m. near the Shapiro Fountain" data-description="Molly Boles, a second-year economics student, studies next to the Shapiro Fountain.<br />ZOE ERSKINE/Daily Bruin" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.9-11am.picB.jpg"></img></a>
+
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.11-2pm.picA.jpg"><img data-title="11:25 a.m. near Royce Hall" data-description="Students study in the Royce Hall archways.<br />AGNIJITA KUMAR/Daily Bruin" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.11-2pm.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.11-2pm.picB.jpg"><img data-title="11:46 a.m. near Kerckhoff Coffeehouse" data-description="Students grab a bite to eat from Kerckhoff Coffeehouse.<br />AGNIJITA KUMAR/Daily Bruin" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.11-2pm.picB.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.11-2pm.picC.jpg"><img data-title="11 a.m. - 2 p.m. near Royce Hall" data-description="Students gather in front of Royce Hall.<br />AGNIJITA KUMAR/Daily Bruin" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.11-2pm.picC.jpg"></img></a>
+
+
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.2-4pm.picA.jpg"><img data-title="3:58 p.m. near Powell Library" data-description="A group of students talk amongst themselves on the Powell Library steps.<br />SIDHAANT SHAH/Daily Bruin Senior Staff" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.2-4pm.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.2-4pm.picB.jpg"><img data-title="2:34 p.m. near Dickson Plaza" data-description="Students walk through Dickson Plaza in front of Royce Hall.<br />ANDREW CHONG/Daily Bruin" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.2-4pm.picB.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.2-4pm.picC.jpg"><img data-title="3:24. near Royce Hall" data-description="The Royce Hall breezeway stands calmly in the afternoon sun.<br />AGNIJITA KUMAR/Daily Bruin" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.2-4pm.picC.jpg"></img></a>
+		
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.4-6pm.picA.jpg"><img data-title="4:15 p.m. near Pauley Pavillion" data-description="Students walk past Pauley Pavilion as they head to the hill.<br />SIDHAANT SHAH/Daily Bruin Senior Staff" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.4-6pm.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.4-6pm.picB.jpg"><img data-title="6 p.m. near Powell Library" data-description="Students walk behind Powell Library as classes come to an end.<br />YIN FU/Daily Bruin" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.4-6pm.picB.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.4-6pm.picC.jpg"><img data-title="5:33 p.m. near Royce Hall" data-description="Angel Kuo, a fourth-year applied mathematics student, poses in her cap and gown as fourth-year biology student Fritz Batiller takes her graduation photos in front of Royce Hall.<br />KAYLA ASATO/Daily Bruin" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.4-6pm.picC.jpg"></img></a>
+		
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.6-9pm.picA.jpg"><img data-title="6:40 p.m. near Wilson Plaza" data-description="A group of students performs an 8-clap at Wilson Plaza.<br />NEIL BEDI/Daily Bruin" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.6-9pm.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.6-9pm.picB.jpg"><img data-title="7:33 p.m. near Powell Library" data-description="Powell Library is filled with students studying for final exams.<br />NEIL BEDI/Daily Bruin" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.6-9pm.picB.jpg"></img></a>
+		
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.10-12.picA.jpg"><img data-title="10:51 p.m. near Caf&eacute; 1919" data-description="A vertical panorama overlooks Caf&eacute; 1919, Hill Top Shop and Sproul Hall.<br />LEXY ATMORE/Daily Bruin Senior Staff" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.10-12.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.10-12pm.picB.jpg"><img data-title="10:15 p.m. near De Neve Late Night" data-description="First-year neuroscience student Matt Haney (left) and first-year chemistry student Chan Kwon study organic chemistry for an upcoming final.<br />LEXY ATMORE/Daily Bruin Senior Staff" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.10-12pm.picB.jpg"></img></a>
+		
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.12-3am.picA.jpg"><img data-title="12:27 a.m. near Janss Steps" data-description="First-year cognitive science student Daniel Lin, front, and fourth-year biochemistry student Chris Koo slide down the hill beside Janss Steps on ice blocks with a group of their dragon boat teammates at 12:27 a.m.<br />JOY JACOBSON/Daily Bruin Senior Staff" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.12-3am.picA.jpg"></img></a>
+		<a href="content/day-in-the-life/web.ph.6.11.DITL.12-3am.picB.jpg"><img data-title="2:08 a.m. near Bruin Caf&eacute;" data-description="Bruin Caf&eacute;, the Hill's late-night staple for a study-break coffee or sandwich, closes at 2 a.m.  Store workers clean the store in the wee hours of the morning.<br />JOY JACOBSON/Daily Bruin Senior Staff" src="content/day-in-the-life/thumb/web.ph.6.11.DITL.12-3am.picB.jpg"></img></a>
 	</div>
 </div>
