3510562636dd0d78cb4ab050647e27ffafeff4b6
Byron Lutz <byronlutz@gmail.com>
2012-06-11 07:05:26 -0700

    Temp fix for incomplete pages

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 $validPages = array(
-	'home' => 'Home',
+/* 	'home' => 'Home', */
 	'map-of-memories' => 'Map of Memories',
 	'summer-sports' => 'Summer Sports',
 	'endangered-languages' => 'Endangered Languages',
 	'day-in-the-life' => 'Day in the Life',
 	'then-and-now' => 'Then and Now',
-	'ae-profiles' => 'a&e Profiles',	
-	'graduation-columns' => 'Graduation Columns',
+/* 	'ae-profiles' => 'a&e Profiles',	 */
+/* 	'graduation-columns' => 'Graduation Columns', */
 );
 
 if(isset($_GET['p']) && array_key_exists($_GET['p'],$validPages)){
@@ -15,6 +15,10 @@ if(isset($_GET['p']) && array_key_exists($_GET['p'],$validPages)){
 }
 else
 	$currentPage = 'home';
+
+// Temp fix for pages not being done
+if($currentPage == 'home')
+	$currentPage = 'summer-sports';
 
 if(!file_exists('pages/'.$currentPage.'.php')) {
 	header("HTTP/1.0 404 Not Found");
