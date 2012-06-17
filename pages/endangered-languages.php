6bd43d5302261b30ff9a9435817c1f24a9beb859
Byron Lutz <byronlutz@gmail.com>
2012-06-17 13:56:18 -0700

    Fixed endangered languages to specify languages that are actually endangered

--- pages/endangered-languages.php
+++ pages/endangered-languages.php
@@ -1,7 +1,7 @@
 <div id="rightContentPane">
 	<div class="endangeredContent">
 		<h1>Spreading the <em>word</em></h1>
-		<h4 style="width:60%;margin-left:auto;margin-right:auto">Below are photos and audio samples of four endangered languages: Bambara, Senaya, Tommo So, and Urdu.</h4>
+		<h4 style="width:60%;margin-left:auto;margin-right:auto">Below are photos and audio samples of "How are you?" in four languages: Bambara, Senaya, Tommo So, and Urdu - including two that are undocumented or endangered.</h4>
 		
 		<h6>Read more at dailybruin.com:</h6>
 		<a href="http://www.dailybruin.com/index.php/article/2012/06/linguistics_graduate_student_strives_to_preserve_fading_languages_through_her_work_at_ucla">Linguistics graduate student strives to preserve fading languages through her work at UCLA</a>
@@ -15,12 +15,12 @@
 		
 		<hr />
 		
-		<h2>Senaya <a class="playButton" id="playSenaya">(Play)</a></h2>
+		<h2><span class="flag">Endangered</span>Senaya <a class="playButton" id="playSenaya">(Play)</a></h2>
 		<img src="content/endangered-languages/senaya.jpg" style="width:500px;"/>
 		
 		<hr />
 		
-		<h2>Tommo So <a class="playButton" id="playTommoso">(Play)</a></h2>
+		<h2><span class="flag">Undocumented</span>Tommo So <a class="playButton" id="playTommoso">(Play)</a></h2>
 		<img src="content/endangered-languages/tommo-so.jpg" style="width:500px;"/>
 		
 		<hr />
